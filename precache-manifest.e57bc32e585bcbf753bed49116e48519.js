369e4eb9085a6104828b31e71d58fafc4f37cc8b
vidhushi <vandanaranavidhu@gmail.com>
2020-02-27 18:56:03 +0530

    Updates

---
+++ precache-manifest.e57bc32e585bcbf753bed49116e48519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301472b3617b27f07e1d59ef42aff3d5",
+    "url": "/currencycalcuator/index.html"
+  },
+  {
+    "revision": "1c4b9eaf4f6865741005",
+    "url": "/currencycalcuator/static/css/main.a2080c3f.chunk.css"
+  },
+  {
+    "revision": "8d1cf3fd306a97b7ca85",
+    "url": "/currencycalcuator/static/js/2.4e7ed056.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/currencycalcuator/static/js/2.4e7ed056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4b9eaf4f6865741005",
+    "url": "/currencycalcuator/static/js/main.d4bb2187.chunk.js"
+  },
+  {
+    "revision": "680813c4e6aac8ff7d5e",
+    "url": "/currencycalcuator/static/js/runtime-main.6165e448.js"
+  }
+]);
